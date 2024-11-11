9e5775d88d33b0b2df800b85eb4bd504846a3c5b
Pedro Nogueira <cmcg89034@gmail.com>
2024-11-11 13:04:38 -0300

    ♻️ Refactored pubsub package to use the custom interface

--- src/cpp_pubsub/src/subscriber_member_function.cpp
+++ src/cpp_pubsub/src/subscriber_member_function.cpp
@@ -2,22 +2,22 @@
 #include <memory>
 
 #include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/string.hpp"
+#include "tutorial_interfaces/msg/num.hpp"
 
 using std::placeholders::_1;
 
 class MinimalSubscriber : public rclcpp::Node {
  public:
   MinimalSubscriber() : Node("minimal_subscriber") {
-    subscription_ = this->create_subscription<std_msgs::msg::String>(
+    subscription_ = this->create_subscription<tutorial_interfaces::msg::Num>(
         "topic", 10, std::bind(&MinimalSubscriber::topic_callback, this, _1));
   }
 
  private:
-  void topic_callback(const std_msgs::msg::String& msg) const {
-    RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg.data.c_str());
+  void topic_callback(const tutorial_interfaces::msg::Num& msg) const {
+    RCLCPP_INFO_STREAM(this->get_logger(), "I heard: '" << msg.num << "'");
   }
-  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
+  rclcpp::Subscription<tutorial_interfaces::msg::Num>::SharedPtr subscription_;
 };
 
 int main(int argc, char* argv[]) {

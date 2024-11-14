be12be615f8afb83369e0fc46502c03a23731e4e
Pedro Nogueira <cmcg89034@gmail.com>
2024-11-14 14:18:25 -0300

    ♻️ Refactored srvcli package to use the custom interface

--- src/cpp_srvcli/src/add_two_ints_server.cpp
+++ src/cpp_srvcli/src/add_two_ints_server.cpp
@@ -1,16 +1,16 @@
 #include <memory>
 
-#include "example_interfaces/srv/add_two_ints.hpp"
 #include "rclcpp/rclcpp.hpp"
+#include "tutorial_interfaces/srv/add_three_ints.hpp"
 
-void add(
-    const std::shared_ptr<example_interfaces::srv::AddTwoInts::Request> request,
-    std::shared_ptr<example_interfaces::srv::AddTwoInts::Response> response) {
-  response->sum = request->a + request->b;
+void add(const std::shared_ptr<tutorial_interfaces::srv::AddThreeInts::Request>
+             request,
+         std::shared_ptr<tutorial_interfaces::srv::AddThreeInts::Response>
+             response) {
+  response->sum = request->a + request->b + request->c;
   RCLCPP_INFO(rclcpp::get_logger("rclcpp"),
-              "Incoming request\na: %ld"
-              " b: %ld",
-              request->a, request->b);
+              "Incoming request\na: %ld b: %ld c: %ld", request->a, request->b,
+              request->c);
   RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "sending back response: [%ld]",
               (long int)response->sum);
 }
@@ -18,11 +18,11 @@ void add(
 int main(int argc, char **argv) {
   rclcpp::init(argc, argv);
   std::shared_ptr<rclcpp::Node> node =
-      rclcpp::Node::make_shared("add_two_ints_server");
-  rclcpp::Service<example_interfaces::srv::AddTwoInts>::SharedPtr service =
-      node->create_service<example_interfaces::srv::AddTwoInts>("add_two_ints",
-                                                                &add);
-  RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Ready to add two ints.");
+      rclcpp::Node::make_shared("add_three_ints_server");
+  rclcpp::Service<tutorial_interfaces::srv::AddThreeInts>::SharedPtr service =
+      node->create_service<tutorial_interfaces::srv::AddThreeInts>(
+          "add_three_ints", &add);
+  RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Ready to add three ints.");
   rclcpp::spin(node);
   rclcpp::shutdown();
 }

59c3d3b202c673791dabbdc36b02cc3d45408262
Pedro Nogueira <cmcg89034@gmail.com>
2025-08-25 16:06:19 -0300

    🔥 the class was in the wrong place!

--- src/bt_hello_world/src/hello_world_bt.cpp
+++ src/bt_hello_world/src/hello_world_bt.cpp
@@ -1,28 +1,7 @@
-#include "hello_world_bt.hpp"
-
 #include "ament_index_cpp/get_package_share_directory.hpp"
 #include "behaviortree_cpp_v3/loggers/bt_cout_logger.h"
 #include "rclcpp/rclcpp.hpp"
-
-SaySomething::SaySomething(const std::string& name,
-                           const BT::NodeConfiguration& config)
-    : BT::SyncActionNode(name, config) {}
-
-BT::PortsList SaySomething::providedPorts() {
-  return {BT::InputPort<std::string>("message")};
-}
-
-BT::NodeStatus SaySomething::tick() {
-  std::string message;
-  if (getInput("message", message)) {
-    RCLCPP_INFO(rclcpp::get_logger("SaySomething"), "%s", message.c_str());
-    return BT::NodeStatus::SUCCESS;
-  } else {
-    RCLCPP_ERROR(rclcpp::get_logger("SaySomething"),
-                 "Missing required input [message]");
-    return BT::NodeStatus::FAILURE;
-  }
-}
+#include "say_something_node.hpp"
 
 int main(int argc, char** argv) {
   rclcpp::init(argc, argv);
